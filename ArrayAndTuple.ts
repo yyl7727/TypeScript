1b25577beb571a2dfba4f95230ae3ad5153e157a
yyl7727 <772788236@qq.com>
2020-10-02 08:58:39 +0800

    基础类型

---
+++ ArrayAndTuple.ts
@@ -0,0 +1,7 @@
+//数组
+let arrOfNumbers: number[] = [1,2,3];
+arrOfNumbers.push(4);
+
+function test(){
+    console.log(arguments);
+}
---
+++ BasicTypes.ts
@@ -0,0 +1,17 @@
+//基本类型
+let isDone: boolean = true;
+
+let age: number = 23;
+
+let firstName: string = "yyl";
+
+let message: string = `hello, ${firstName}`;
+
+let u: undefined = undefined;
+
+let n: null = null;
+
+//any
+let notSure: any = 4;
+notSure = 'maybe a string';
+notSure = true;
