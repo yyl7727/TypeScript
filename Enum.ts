12cf6ef2e9e81a6b868a19a9ce93dad4bd98ee56
yyl7727 <772788236@qq.com>
2020-10-02 10:16:21 +0800

    枚举

---
+++ Enum.ts
@@ -0,0 +1,12 @@
+enum Direction{
+    up = 'UP',
+    down = 'DOWN',
+    left ='LEFT',
+    right = 'RIGHT'
+}
+
+const value = 'UP';
+if(value == Direction.up){
+    console.log('up up up');
+    
+}
